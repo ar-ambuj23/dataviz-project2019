182ecb171f147acf714991f4fb5fd7c05ba90c92
ar-ambuj23 <ar.ambuj23@gmail.com>
2019-11-06 12:45:22 -0700

    file str defined

--- code/js/PollutantList.js
+++
@@ -1,8 +0,0 @@
-
-/* ENUM for the pollutants in dataset. */
-let pollutants = {
-	"NO2" : "NO2",
-	"SO2" : "SO2",
-	"CO"  : "CO",
-	"O3"  : "O3"
-}
--- code/js/TimePeriod.js
+++
@@ -1,6 +0,0 @@
-
-/* ENUM for the timeperiods we're offering. */
-let timePeriod = {
-	'YEAR' : "year",
-	'MONTH' : "month"
-}
