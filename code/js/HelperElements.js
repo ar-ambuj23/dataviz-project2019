6ffc492d355dfd29a19ba0a8ef2fd8c2fab281f2
Shaurya <shauryasahai@gmail.com>
2019-11-08 18:26:40 -0700

    added events and events handlers for slider and dropdown

--- code/js/HelperElements.js
+++ code/js/HelperElements.js
@@ -9,14 +9,18 @@ class HelperElements {
  * 3. Pollutant Dropdown.
  * 4. Info Box.
  */
-    constructor(pollutionData) {
+    constructor(pollutionData, updatePollutant, updateTime) {
         console.log("Inside the Helper Class Constructor")
         
         // Making a list of options for drop down
-        this.pollutantList = ["All","Corbon Monoxide", "Sulphur Dioxide","Nitrous Oxide","Ozone"]
+        this.pollutantList = ["All","Carbon Monoxide", "Sulphur Dioxide","Nitrous Oxide","Ozone"]
         
         //Making Pollution Data a class variable
         this.pollutionData = pollutionData;
+
+        this.updatePollutant = updatePollutant;
+
+        this.updateTime = updateTime;
 
         // Calling Helper Methods
         this.drawTimeSlider();
@@ -30,17 +34,27 @@ class HelperElements {
      */
     drawTimeSlider() {
 
+        let that = this;
+
         // Making a scale for slider
-        let yearScale = d3.scaleLinear().domain([2000, 2016]).range([30, 730]); 
+        let timeScale = d3.scaleLinear().domain([2000, 2016]).range([30, 730]); 
 
         console.log('Inside drawTimeSlider')
         d3.select("#slider")
-        .append('div').classed('slider-wrap', true)
-        .append('input').classed('slider', true)
-        .attr('type', 'range')
-        .attr('min', 2000)
-        .attr('max', 2016)
-        .attr('value', 2004);
+            .append('div').classed('slider-wrap', true)
+            .append('input').classed('slider', true)
+            .attr('type', 'range')
+            .attr('min', 2000)
+            .attr('max', 2016)
+            .attr('value', 2004)
+            .attr('id', 'time-slider');
+
+        let timeSlider = d3.select('#time-slider');
+
+        timeSlider.on('input', function() {
+            let time = this.value;
+            that.updateTime(time);
+        });
     }
 
     /**
@@ -56,19 +70,25 @@ class HelperElements {
      * pollutants and throws related events.
      */
     drawDropdown(pollutantList) {
-        // console.log(this.pollutionData)
 
-    var option_select = d3.select("#buttons")
-            .append("div")
-            .append("select")
-            .attr("id", "pollutantSelector")
+        let that = this;
 
-  for (var i = 0; i < this.pollutantList.length; i++) {
-        var opt = option_select.append("option")
-        .attr("value", this.pollutantList[i])
-        .text(this.pollutantList[i]);
-  }
-}
+        let option_select = d3.select("#buttons")
+                .append("div")
+                .append("select")
+                .attr("id", "pollutantSelector")
+
+        for (var i = 0; i < this.pollutantList.length; i++) {
+            var opt = option_select.append("option")
+            .attr("value", this.pollutantList[i])
+            .text(this.pollutantList[i]);
+        }
+
+        option_select.on('change', function(d, i) {
+            let selectedPollutant = this.options[this.selectedIndex].value;
+            that.updatePollutant(selectedPollutant);
+        });
+    }
 
     /**
      * draws the info-box when a 
--- code/js/script.js
+++ code/js/script.js
@@ -1,4 +1,11 @@
- 
+
+function updateTime(time) {
+    console.log(time);
+}
+
+function updatePollutant(pollutant) {
+    console.log(pollutant);
+}
     /**
      * Loads in the us-pollution-data.json
      */
@@ -8,13 +15,14 @@ d3.json('https://d3js.org/us-10m.v1.json').then( usMapData => {
     d3.json('data/pollution_data_rolled_up.json').then( pollutionData => {
 
             //Create Map Object
-            let mapObj = new Map(usMapData,null);
+            let mapObj = new Map(usMapData, pollutionData);
             mapObj.createMap();
 
             //Creating all the Helper Elements
-            let helperObj = new HelperElements(pollutionData);
+            let helperObj = new HelperElements(pollutionData, updatePollutant, updateTime);
 
-})
+    })
 
 });
+
     
